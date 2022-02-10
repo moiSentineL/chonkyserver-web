1b17cf0a97538359a417434da41a076dae28155b
moisentinel <nibir.patowary@gmail.com>
2022-02-10 10:55:20 +0530

    Better Small Screen UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,58 +9,67 @@ const Home = () => {
             </Head>
 
             <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-                <h1 className="mt-5 text-3xl font-bold sm:text-4xl lg:-mt-5 lg:text-6xl">
+                <h1 className="mt-5 text-3xl font-bold sm:text-6xl lg:-mt-5 lg:text-6xl">
                     Welcome to ChonkyServer
                 </h1>
 
                 <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+                    
                     <a
                         href="/more-info"
-                        className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-white focus:text-white"
+                        className="mt-6 w-96 px-5 sm:px-0 text-left hover:text-white focus:text-white"
                     >
-                        <h3 className="text-2xl font-bold">
-                            More Information &rarr;
-                        </h3>
-                        <p className="mt-4 text-xl">
-                            Click here for more information about the server.
-                        </p>
+                        <div className="rounded-xl border p-5">
+                            <h3 className="text-2xl font-bold">
+                                More Information &rarr;
+                            </h3>
+                            <p className="mt-4 text-xl">
+                                Click here for more information about the server.
+                            </p>
+                        </div> 
                     </a>
 
                     <a
                         href="/connections"
-                        className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-white focus:text-white"
+                        className="mt-6 w-96 px-5 sm:px-0 text-left hover:text-white focus:text-white"
                     >
-                        <h3 className="text-2xl font-bold">
-                            Join / Connect to Server &rarr;
-                        </h3>
-                        <p className="mt-4 text-xl">
-                            Click here for IP Address and Port.
-                        </p>
-                        <br />
+                        <div className="rounded-xl border p-5">
+                            <h3 className="text-2xl font-bold">
+                                Join / Connect to Server &rarr;
+                            </h3>
+                            <p className="mt-4 text-xl">
+                                Click here for IP Address and Port.
+                            </p>
+                            <br />
+                        </div>
                     </a>
 
                     <a
                         href="/tutorials"
-                        className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-white focus:text-white"
+                        className="mt-6 w-96 px-5 sm:px-0 text-left hover:text-white focus:text-white"
                     >
-                        <h3 className="text-2xl font-bold">Tutorials &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Click here to see how the plugins in the server
-                            work.
-                        </p>
+                        <div className="rounded-xl border p-5">
+                            <h3 className="text-2xl font-bold">Tutorials &rarr;</h3>
+                            <p className="mt-4 text-xl">
+                                Click here to see how the plugins in the server
+                                work.
+                            </p>
+                        </div>
                     </a>
 
                     <a
                         href="/contact"
-                        className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-white focus:text-white"
+                        className="mt-6 w-96 px-5 sm:px-0 text-left hover:text-white focus:text-white"
                     >
-                        <h3 className="text-2xl font-bold">
-                            Contact Us &rarr;
-                        </h3>
-                        <p className="mt-4 text-xl">
-                            Click here for more support.
-                        </p>
-                        <br />
+                        <div className="rounded-xl border p-5">
+                            <h3 className="text-2xl font-bold">
+                                Contact Us &rarr;
+                            </h3>
+                            <p className="mt-4 text-xl">
+                                Click here for more support.
+                            </p>
+                            <br />
+                        </div>
                     </a>
                 </div>
             </main>
